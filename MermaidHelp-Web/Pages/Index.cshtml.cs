669c3205da411f5a10dbecefeb40225295b9c2e6
史悦 <shiyue@hdzxbj.com>
2024-05-30 14:15:10 +0800

    使用 mermaid.ink 导出图片

--- MermaidHelp-Web/Pages/Index.cshtml.cs
+++ MermaidHelp-Web/Pages/Index.cshtml.cs
@@ -2,9 +2,13 @@ using FineUICore;
 using MermaidHelp.Code;
 using Microsoft.AspNetCore.DataProtection.KeyManagement;
 using Microsoft.AspNetCore.Html;
+using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.Rendering;
 using Microsoft.AspNetCore.SignalR.Protocol;
 using Newtonsoft.Json.Linq;
+using ShimSkiaSharp;
+using SkiaSharp;
+using Svg.Skia;
 using System.Threading.Tasks;
 
 namespace MermaidHelp.Pages
@@ -53,6 +57,65 @@ namespace MermaidHelp.Pages
             string output = normalizedInput.Replace("\n", "\\n");
 
             return output;
+        }
+
+        public IActionResult OnPostExportToExcel(string svgContent)
+        {
+            if (string.IsNullOrWhiteSpace(svgContent))
+            {
+                return BadRequest("SVG content is required.");
+            }
+
+            var imageBytes = ConvertSvgToPng(svgContent);
+
+            if (imageBytes == null)
+            {
+                return StatusCode(500, "Error converting SVG to PNG.");
+            }
+
+            return File(imageBytes, "image/png", "image.png");
+        }
+
+        private byte[] ConvertSvgToPng(string svgContent)
+        {
+            //Encoding.RegisterProvider(CodePagesEncodingProvider.Instance);
+            //var encoding = Encoding.GetEncoding("GB2312");
+            //var bytes = encoding.GetBytes(svgContent);
+
+            //using (var svgstream = new MemoryStream(bytes))
+            using (var svgstream = new MemoryStream(System.Text.Encoding.UTF8.GetBytes(svgContent)))
+            using (var svg = new SKSvg())
+            {
+                svg.Load(svgstream);
+
+                var bitmap = new SKBitmap((int)svg.Picture.CullRect.Width, (int)svg.Picture.CullRect.Height);
+                using (var canvas = new SkiaSharp.SKCanvas(bitmap))
+                {
+                    canvas.Clear(SKColors.White);
+                    // 加载系统黑体字体
+                    var typeface = SkiaSharp.SKTypeface.FromFamilyName("SimHei");
+
+                    var paint = new SkiaSharp.SKPaint
+                    {
+                        Typeface = typeface,
+                        TextSize = 24,
+                        IsAntialias = true,
+                        Color = SKColors.Black,
+                        TextEncoding = SkiaSharp.SKTextEncoding.Utf8
+                    };
+                    var fontPath = Path.Combine(FineUICore.PageContext.WebRootPath, "res", "SIMHEI.TTF");
+                    paint.Typeface = SKFontManager.Default.CreateTypeface(fontPath);//字体
+                    canvas.DrawPicture(svg.Picture, paint);
+                }
+
+                using (var image = SkiaSharp.SKImage.FromBitmap(bitmap))
+                using (var data = image.Encode(SKEncodedImageFormat.Png, 100))
+                using (var stream = new MemoryStream())
+                {
+                    data.SaveTo(stream);
+                    return stream.ToArray();
+                }
+            }
         }
     }
 }
